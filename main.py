a76e59672b7f85947f240e3ea4a11fecb4865d84
hrdu_ <ragnarbergseth@yahoo.no>
2023-04-15 18:49:18 +0200

    Updated Main.py

--- main.py
+++ main.py
@@ -1,38 +1,33 @@
-from discord import app_commands, Intents, Client, Interaction
+import os
+import discord
+from discord import ApplicationCommand, InteractionResponseType
+from discord.ext import commands
 from Commands.info_command import on_message
 from Restarter.keep_alive import keep_alive
 
-import discord
-import os
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 
-class Executive(Client):
-    def __init__(self, *, intents: Intents):
-        super().__init__(intents=intents)
-        self.tree = app_commands.CommandTree(self)
+async def _init_command_response(interaction: discord.Interaction) -> None:
+    await interaction.response.send_message(
+        f"Hi **{interaction.user}**, thank you for saying hello to me."
+    )
 
 
-client = Executive(intents=Intents.all())
+@bot.slash_command(name="hello", description="Says hello or something")
+async def hello(ctx: commands.Context):
+    await _init_command_response(ctx)
 
 
-async def _init_command_response(interaction: Interaction) -> None:
-    await interaction.response.send_message("\n".join([
-        f"Hi **{interaction.user}**, thank you for saying hello to me."]))
-
-
-@client.tree.command()
-async def hello(interaction: Interaction):
-    """Says hello or something"""
-    await _init_command_response(interaction)
-
-
-client.event(on_message)
+bot.event(on_message)
 
 keep_alive()
 
 try:
-    client.run(os.getenv("token"))
+    bot.run(os.getenv("token"))
 except discord.errors.HTTPException:
     print("\n\n\nBLOCKED BY RATE LIMITS\nRESTARTING NOW\n\n\n")
-    os.system('kill 1')
+    os.system("kill 1")
     os.system("python restarter.py")
